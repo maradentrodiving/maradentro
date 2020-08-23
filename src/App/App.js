351c3e958a9b722c3a70d28782abc48c79c6dd5c
Jakub Šťastný <jakub.stastny.pt+web@gmail.com>
2020-08-23 22:27:24 +0000

    Basic infrastructure

---
+++ src/App/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Header from '../Header/Header'
+import Footer from '../Footer/Footer'
+
+export default function App () {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  )
+}
+
+// <a
+//   className="App-link"
+//   href="https://reactjs.org"
+//   target="_blank"
+//   rel="noopener noreferrer"
+// >
