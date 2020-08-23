ff0e790c5d707f633a5a40f004f7525c408fcd7f
Jakub Šťastný <jakub.stastny.pt+web@gmail.com>
2020-08-23 23:27:46 +0000

    Footer contact info

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import styles from './Footer.module.css'
+import { RiWhatsappFill as WhatsAppIcon } from 'react-icons/ri'
+import { TiMail as EmailIcon } from 'react-icons/ti'
+import { contactEmail, contactNumberShop, contactNumberOscar } from '../constants'
 
 export default function Footer () {
   return (
     <footer className={styles.footer}>
+      <ul className={styles.contactList}>
+        <li><WhatsAppIcon className={styles.whatsappIcon} /> {contactNumberShop}</li>
+        <li><WhatsAppIcon className={styles.whatsappIcon} /> {contactNumberOscar}</li>
+        <li><EmailIcon className={styles.icon} />{contactEmail}</li>
+      </ul>
     </footer>
   )
 }
---
+++ src/constants.js
@@ -0,0 +1,3 @@
+export const contactEmail = 'info@maradentrodiving.com'
+export const contactNumberShop = '+52 983 116 9585'
+export const contactNumberOscar = '+52 983 125 5207'
