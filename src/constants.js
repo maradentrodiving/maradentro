afe4a7e34124daee57f72e9bba903a4f41320c1b
Jakub Šťastný <jakub.stastny.pt+web@gmail.com>
2020-08-24 16:14:43 +0000

    Footer & social icons

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,9 @@
+/* Contact info */
 export const contactEmail = 'info@maradentrodiving.com'
 export const contactNumberShop = '+52 983 116 9585'
 export const contactNumberOscar = '+52 983 125 5207'
+
+/* Social media links */
+export const tripAdvisorURL = 'https://www.tripadvisor.com/Attraction_Review-g499450-d4185835-Reviews-Mar_Adentro_Diving-Mahahual_Costa_Maya_Yucatan_Peninsula.html'
+export const facebookURL = 'https://web.facebook.com/maradentrodivingmajahual'
+export const instagramURL = 'https://www.instagram.com/maradentrodiving/'
