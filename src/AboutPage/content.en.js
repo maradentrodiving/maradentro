57bd8ebceba599127b9baddea33343bb68d30749
Jakub Šťastný <jakub.stastny.pt+git@gmail.com>
2020-09-11 15:08:03 +0000

    The about page

--- src/AboutPage/content.en.js
+++ src/AboutPage/content.en.js
@@ -3,6 +3,37 @@ import React from 'react'
 
 export default () => (
   <>
-    ABOUT EN
+    <h1>OUR AGENCY</h1>
+    <h2>MAR ADENTRO DIVING</h2>
+
+    <p>
+      Liuba, our cute mermaid, will welcome you by the km 0,3 of the coastal road to Xcalak, not far from the lighthouse, where the Mexican Caribbean unfolds one of its alluring landscapes.
+    </p>
+
+    <p>
+      Mar Adentro Diving Center was established on 13th December 2011 south of the village, to be relocated after hurricane Ernesto. Today is adjacent to Señor Frog’s bar and restaurant and it boasts Majahual’s finest beach, stretching just in front: the Blue Bay Beach Club. Besides, we are very close to one of most interesting local dive sites: Los Escalones.
+    </p>
+
+    <p>
+      Our business hours: 8:30 am to 6:30 pm, seven days a week. But we are flexible and eager to adapt to your needs. Count on us!
+    </p>
+
+    <h2>FACILITIES</h2>
+
+    <p>
+      Our facilities are functional and new, so is the equipment.
+    </p>
+
+    <h2>PRIORITIES</h2>
+
+    <p>
+      In Mar Adentro we have four top priorities: the good health of the reef , the quality of teaching,safety and fun. We also believe scuba diving should belong to divers and not to the diving industry. We love to make friends.
+    </p>
+
+    <h2>MISSION</h2>
+
+    <p>
+      We will always go the extra mile to make your stay in Majahual as pleasant as possible.
+    </p>
   </>
 )
--- src/AboutPage/content.es.js
+++ src/AboutPage/content.es.js
@@ -3,6 +3,37 @@ import React from 'react'
 
 export default () => (
   <>
-    ABOUT ES
+    <h1>OUR AGENCY</h1>
+    <h2>MAR ADENTRO DIVING</h2>
+
+    <p>
+      ... traduzca ...
+    </p>
+
+    <p>
+      ... traduzca ...
+    </p>
+
+    <p>
+      ... traduzca ...
+    </p>
+
+    <h2>FACILITIES</h2>
+
+    <p>
+      ... traduzca ...
+    </p>
+
+    <h2>PRIORITIES</h2>
+
+    <p>
+      ... traduzca ...
+    </p>
+
+    <h2>MISSION</h2>
+
+    <p>
+      ... traduzca ...
+    </p>
   </>
 )
