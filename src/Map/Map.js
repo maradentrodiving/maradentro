e80455f19753559f4de275d558958f13eaef4450
Jakub Šťastný <jakub.stastny.pt+git@gmail.com>
2020-09-11 15:38:45 +0000

    Fix property names

--- src/Map/Map.js
+++ src/Map/Map.js
@@ -15,10 +15,10 @@ export default function Map () {
           height="276"
           id="gmap_canvas"
           src="https://maps.google.com/maps?q=Mar%20Adentro%20Diving%2C%20Mahahual%2C%20Mexico&t=&z=15&ie=UTF8&iwloc=&output=embed"
-          frameborder="0"
+          frameBorder="0"
           scrolling="no"
-          marginheight="0"
-          marginwidth="0">
+          marginHeight="0"
+          marginWidth="0">
         </iframe>
       </div>
     </div>
