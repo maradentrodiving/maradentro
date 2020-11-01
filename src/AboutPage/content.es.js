d430415aa37dc77e0403cfd55da030480867585e
Jakub Šťastný <jakub.stastny.pt+git@gmail.com>
2020-11-01 12:55:45 +0000

    About us

--- src/AboutPage/content.es.js
+++ src/AboutPage/content.es.js
@@ -3,37 +3,17 @@ import React from 'react'
 
 export default () => (
   <>
-    <h1>OUR AGENCY</h1>
-    <h2>MAR ADENTRO DIVING</h2>
-
+    <h1>¡Conócenos!</h1>
     <p>
-      ... traduzca ...
+      Liuba, nuestra bella sirena, te dará la bienvenida en el kilómetro 0,5 del camino costero de Mahahual a Xcalak, o malecón, no lejos del faro, donde el Caribe mexicano nos regala una de sus sugestivas panorámicas de palmeras y azules que hacen las mejores postales de Quintana Roo. Y bajo las olas, aguarda su mejor paisaje: la Gran Barrera Maya.
     </p>
 
     <p>
-      ... traduzca ...
+      El centro de buceo Mar Adentro Diving, fue fundado el 13 de diciembre de 2011 al sur del pueblo, pero los daños del huracán Ernesto (agosto 2012) obligaron a buscarle un nuevo emplazamiento. Durante algunos años estuvo anexo al club Beach Escape para reubicarse en diciembre de 2017 en su sede actual, entre los apartamentos Aquastar y el Hotel Quinto Sole. Disponemos de un muelle de madera para hacer más cómodo el acceso a las barcas; Blue Kay -la playa más hermosa de Mahahual- es nuestro vecino y al frente del centro nos esperan dos de nuestros sitios de buceo favoritos: Escalones y Las perlas.
     </p>
 
     <p>
-      ... traduzca ...
-    </p>
-
-    <h2>FACILITIES</h2>
-
-    <p>
-      ... traduzca ...
-    </p>
-
-    <h2>PRIORITIES</h2>
-
-    <p>
-      ... traduzca ...
-    </p>
-
-    <h2>MISSION</h2>
-
-    <p>
-      ... traduzca ...
+      Mar Adentro es un centro y escuela de buceo (<a href="/servicios">cursos</a> <a href="https://padi.com">PADI</a>) con dos prioridades: la buena salud del arrecife de coral y la calidad en la enseñanza. Creemos que el buceo debe ser para los buceadores y no para la industria del buceo. Nuestros horarios son muy flexibles y siempre puedes contar con que vamos a darlo todo para que tu estancia en Mahahual sea lo más placentera posible.
     </p>
   </>
 )
